682da48c62685b3786e80299f48dd31f7ac5c366
IbrahimIsazade <ibrahimisazade264@gmail.com>
2024-05-23 12:57:57 +0400

    Update V7.1 * Student search system finished

--- assets/js/student-list.js
+++ assets/js/student-list.js
@@ -1,7 +1,4 @@
 const table = document.querySelector(".students-data");
-const idInpt = document.querySelector(".input_id");
-const nameInpt = document.querySelector(".input_name");
-const phoneInpt = document.querySelector(".input_phone");
 const searchBtn = document.querySelector(".search-btn");
 
 function fillTableWith(data) {
@@ -35,20 +32,100 @@ fetch("http://localhost:3000/students")
 .then(response => response.json())
 .then(data => fillTableWith(data))
 
-searchBtn.onclick = () => {
+// Debounce function
+function debounce(func, wait) {
+    let timeout;
+    return function(...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func();
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+}
 
-    if (idInpt.value == "" && 
-        nameInpt.value == "" && 
-        phoneInpt.value == "") {
-        console.log("Search is empty");
+// Search function
+function performSearch() {
+    const idInpt = document.querySelector(".input_id").value;
+    const nameInpt = document.querySelector(".input_name").value;
+    const phoneInpt = document.querySelector(".input_phone").value;
+
+    if (idInpt == "" && nameInpt == "" && phoneInpt == "") {
+        fetch("http://localhost:3000/students")
+        .then(response => response.json())
+        .then(data => fillTableWith(data))
+        return;
     }
-
-    fetch("http://localhost:3000/students")
-    .then(response => response.json())
-    .then(data => fillTableWith(data))
-    
+    else if (idInpt != "") {
+        fetch(`http://localhost:3000/students/${idInpt}`)
+        .then(response => {
+            if (!response.ok) {
+                if (response.status === 404) {
+                    throw new Error('Student not found (404)');
+                } else if (response.status === 500) {
+                    throw new Error('Server error (500)');
+                } else {
+                    throw new Error(`Unexpected error: ${response.status}`);
+                }
+            }
+            return response.json();
+        })
+        .then(data => fillTableWith([data]))
+        .catch(error => alert(error.message));
+        
+        return;
+    }
+    else if (phoneInpt != "") {
+        fetch(`http://localhost:3000/students/?mobileNumber=${phoneInpt}`)
+        .then(response => {
+            if (!response.ok) {
+                if (response.status === 404) {
+                    throw new Error('Student not found (404)');
+                } else if (response.status === 500) {
+                    throw new Error('Server error (500)');
+                } else {
+                    throw new Error(`Unexpected error: ${response.status}`);
+                }
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.length > 1) {
+                fillTableWith(data)
+            } else {
+                fillTableWith([data])
+            }
+        })
+        return;
+    }
+    else if (nameInpt != "") {
+        fetch(`http://localhost:3000/students/?name=${nameInpt}`)
+        .then(response => {
+            if (!response.ok) {
+                if (response.status === 404) {
+                    throw new Error('Student not found (404)');
+                } else if (response.status === 500) {
+                    throw new Error('Server error (500)');
+                } else {
+                    throw new Error(`Unexpected error: ${response.status}`);
+                }
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.length > 1) {
+                fillTableWith(data)
+            } else {
+                fillTableWith([data])
+            }
+        })
+        return;
+    }
 
 }
 
+// Wrap the search function with debounce
+const debouncedSearch = debounce(performSearch, 1000);
 
-
+searchBtn.onclick = debouncedSearch;
