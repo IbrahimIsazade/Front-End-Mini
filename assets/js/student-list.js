9ce933e8877601161a2a8e65f7a1bd57619bf712
IbrahimIsazade <ibrahimisazade264@gmail.com>
2024-05-23 11:53:58 +0400

    Update V7 * Login finished * Blog list finished * Search system in progress

--- assets/js/student-list.js
+++ assets/js/student-list.js
@@ -1,8 +1,12 @@
 const table = document.querySelector(".students-data");
+const idInpt = document.querySelector(".input_id");
+const nameInpt = document.querySelector(".input_name");
+const phoneInpt = document.querySelector(".input_phone");
+const searchBtn = document.querySelector(".search-btn");
 
-fetch("http://localhost:3000/students")
-.then(response => response.json())
-.then(data => {
+function fillTableWith(data) {
+    table.innerHTML = ``;
+
     data.forEach(item => {
         const valuesArray = Object.values(item);
         const studentRow = document.createElement("tr");
@@ -13,14 +17,38 @@ fetch("http://localhost:3000/students")
 
             studentRow.appendChild(prop);
         });
-        studentRow.innerHTML += `<td class="action-buttons flex items-center justify-between">
-        <a href="./profile.html" class="action-btn flex justify-center items-center">
-            <i class="fa-regular fa-eye"></i>
-        </a>
-        <a href="#" class="action-btn flex justify-center items-center">
-            <i class="fa-regular fa-pen-to-square"></i>
-        </a>
-    </td>`;
+        studentRow.innerHTML += 
+        `<td class="action-buttons flex items-center justify-between">
+            <a href="./profile.html" class="action-btn flex justify-center items-center">
+                <i class="fa-regular fa-eye"></i>
+            </a>
+            <a href="#" class="action-btn flex justify-center items-center">
+                <i class="fa-regular fa-pen-to-square"></i>
+            </a>
+        </td>`;
+
         table.appendChild(studentRow);
     });
-})
+}
+
+fetch("http://localhost:3000/students")
+.then(response => response.json())
+.then(data => fillTableWith(data))
+
+searchBtn.onclick = () => {
+
+    if (idInpt.value == "" && 
+        nameInpt.value == "" && 
+        phoneInpt.value == "") {
+        console.log("Search is empty");
+    }
+
+    fetch("http://localhost:3000/students")
+    .then(response => response.json())
+    .then(data => fillTableWith(data))
+    
+
+}
+
+
+
